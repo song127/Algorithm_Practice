8c7b1406ff547355e1aca222ddd615d8bb17bbb5
song127 <songsstudyadress@gmail.com>
2024-03-20 17:23:06 +0900

    add frame

--- TwoPointer/SubTotal1.java
+++ TwoPointer/SubTotal1.java
@@ -1,7 +1,25 @@
 package TwoPointer;
 
+import java.util.Scanner;
+
 public class SubTotal1 {
+
+  static int[] arr;
+  static int minLength = 0;
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int s = sc.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    solution();
+  }
+
+  private static void solution() {
 
   }
 }
